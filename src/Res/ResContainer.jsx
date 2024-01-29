deb32c6b1c26b454502eb8384b959a6b31db640d
devmalya99 <debmalyamazumdar99@gmail.com>
2024-01-30 03:45:29 +0530

    search and filter fixed

--- src/Res/ResContainer.jsx
+++ src/Res/ResContainer.jsx
@@ -1,79 +1,96 @@
-import React, { useState, useEffect } from 'react';
-import ResCard from './ResCard';
-import Shimmer from '../Component/Shimmer';
+import React, { useState, useEffect } from "react";
+import ResCard from "./ResCard";
+import Shimmer from "../Component/Shimmer";
 
 const ResContainer = () => {
- 
   const [newRes, setNewRes] = useState([]);
-  
+  const [filteredRes,setFilteredRes]=useState([]);
+  const [searchText,setSearchText]=useState("")
+
   useEffect(() => {
     fetchData();
   }, []); // useEffect will get applied once the component is mounted/rendered
 
-
   const fetchData = async () => {
     try {
-       
-       const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.572646&lng=88.36389500000001&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
-       const json = await data.json();
-       console.log(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
-       setNewRes(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
-       
-    } 
-    
-    catch (error) 
-    {
-       console.error("Error fetching data:", error);
-    }  
-    
-   };
-   
+      const data = await fetch(
+        "https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.572646&lng=88.36389500000001&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+      );
+      const json = await data.json();
+      console.log(
+        json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle
+          ?.restaurants
+      );
+      setNewRes(
+        json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle
+          ?.restaurants
+      );
 
-   
-
-  
+      setFilteredRes(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle
+        ?.restaurants)
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
 
   const handleFilterClick = () => {
     const filteredList = newRes.filter((item) => item.info.avgRating > 4.1);
     console.log(filteredList);
-    setNewRes(filteredList);
+    setFilteredRes(filteredList);
   };
+
+  const handleSearch =()=>
+  {
+   const filteredRes= newRes.filter((res)=>
+    {
+      return res.info.name.toLowerCase().includes(searchText.toLowerCase())
+    })
+    console.log(searchText)
+    setFilteredRes(filteredRes)
+  }
 
   const handleResetClick = () => {
     // Reset the filter
     fetchData();
   };
 
+  return (newRes.length === 0)? (
+    <Shimmer />
+  ) : (
+    <div className="bg-gray-800 p-8 sm:p-12 lg:p-16">
+      <div className="flex space-x-4">
+        <input type="text" 
+        placeholder="Search"
+        value={searchText}
+        onChange={(e)=>{setSearchText(e.target.value)}}
+         />
+        <button 
+        onClick={handleSearch}
+        className="bg-gradient-to-r from-pink-500 to-blue-700 hover:from-blue-700 hover:to-pink-900 text-white font-bold py-2 px-4 rounded focus:outline-none focus:ring focus:border-blue-300 transition duration-300">
 
- 
-  return newRes.length===0?<Shimmer/>:
-   (<div className='bg-gray-800 p-8 sm:p-12 lg:p-16'>
-   <div className="flex space-x-4">
-<button
-onClick={handleFilterClick}
-className="bg-gradient-to-r from-blue-500 to-blue-700 hover:from-blue-700 hover:to-blue-900 text-white font-bold py-2 px-4 rounded focus:outline-none focus:ring focus:border-blue-300 transition duration-300"
->
-Filter Top Restaurants
-</button>
-<button
-onClick={handleResetClick}
-className="bg-gradient-to-r from-gray-500 to-gray-700 hover:from-gray-700 hover:to-gray-900 text-white font-bold py-2 px-4 rounded focus:outline-none focus:ring focus:border-gray-300 transition duration-300"
->
-Reset Filter
-</button>
-</div>
-<div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6'>
-   
-  { newRes.map((eachRes) => (
-     <ResCard key={eachRes.info.id} resData={eachRes} />
-   ))}
- 
-</div>
-</div>)
+          Search
+        </button>
 
-   
-    
-  
+        <button
+          onClick={handleFilterClick}
+          className="bg-gradient-to-r from-blue-500 to-blue-700 hover:from-blue-700 hover:to-blue-900 text-white font-bold py-2 px-4 rounded focus:outline-none focus:ring focus:border-blue-300 transition duration-300"
+        >
+          Filter Top Restaurants
+        </button>
+        <button
+          onClick={handleResetClick}
+          className="bg-gradient-to-r from-gray-500 to-gray-700 hover:from-gray-700 hover:to-gray-900 text-white font-bold py-2 px-4 rounded focus:outline-none focus:ring focus:border-gray-300 transition duration-300"
+        >
+          Reset Filter
+        </button>
+      </div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+        {filteredRes.map((eachRes) => (
+          <ResCard key={eachRes.info.id} resData={eachRes} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default ResContainer;
--- src/SearchBar.jsx
+++ src/SearchBar.jsx
@@ -13,12 +13,11 @@ const SearchBar = () => {
               type="search"
               id="default-search"
               className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="Search Mockups, Logos..."
+              placeholder="Search Restaurants"
               required
             />
             <button
               type="submit"
-              onClick={filterRes}
               className="text-white absolute right-2.5 bottom-2.5 
               bg-blue-700 hover:bg-blue-800 focus:ring-4 
               focus:outline-none focus:ring-blue-300 font-medium 
